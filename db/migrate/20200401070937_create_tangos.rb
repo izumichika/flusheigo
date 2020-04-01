e32d5f3a6718a6c4bb2450ce325e3bb722611b6b
izumichika <chikara0707@outlook.jp>
2020-04-01 16:15:14 +0900

    マイグレーション
    単語モデルのマイグレーションを設定

--- db/migrate/20200401070937_create_tangos.rb
+++ db/migrate/20200401070937_create_tangos.rb
@@ -1,7 +1,9 @@
 class CreateTangos < ActiveRecord::Migration[5.2]
   def change
     create_table :tangos do |t|
-
+      t.string :question, null: false
+      t.string :anser, null: false
+      t.string :result
       t.timestamps
     end
   end
