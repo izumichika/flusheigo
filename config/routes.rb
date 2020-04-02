b24514173ff7ced307893b5372adf30a08e0089f
izumichika <chikara0707@outlook.jp>
2020-04-02 12:08:10 +0900

    add routes
    削除を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "tangos#index"
-  resources :tangos, only: [:index, :new, :create]
+  resources :tangos, only: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
