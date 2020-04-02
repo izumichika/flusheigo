bafefbca4d92a0481bec44e62101dbe1d2ebe82d
izumichika <chikara0707@outlook.jp>
2020-04-02 12:10:45 +0900

    add action
    createアクションを作成

--- app/controllers/tangos_controller.rb
+++ app/controllers/tangos_controller.rb
@@ -9,7 +9,13 @@ class TangosController < ApplicationController
   end
 
   def create
-    
+    Tango.create(tango_params)
   end
+
   
+  
+  private
+  def tango_params
+    params.require(:tango).permit(:question, :anser, :result)
+  end
 end
