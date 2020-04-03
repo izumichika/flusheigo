f72b5a7ee53306b5362a66bfb7b7084efa072bf6
izumichika <chikara0707@outlook.jp>
2020-04-03 21:48:17 +0900

    fix js
    特定のページのみjsが反応するようにルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   root "tangos#index"
   resources :tangos, only: [:index, :new, :create, :destroy, :show] do
-    resources :tests, only: [:index] do
-      collection do
-        get 'test', to: "tests#test"
-        post 'mark', to: "tests#mark"
-      end
+    # resources :tests, only: [:index] do
+    #   collection do
+    #     get 'test', to: "tests#test"
+    #     post 'mark', to: "tests#mark"
+    #   end
+    # end
+  end
+  resources :tests, only: [:index] do
+    collection do
+      get 'test', to: "tests#test"
+      post 'mark', to: "tests#mark"
     end
   end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
