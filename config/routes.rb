798026c6ec68cb9bc854376dc9aa28801a474fdd
izumichika <chikara0707@outlook.jp>
2020-04-10 14:20:03 +0900

    修正
    不要な行を削除
    ヘッダータイトルのビュー崩れを修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,4 @@ Rails.application.routes.draw do
       post 'mark', to: "tests#mark"
     end
   end
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
