fb7918ea590c7a1c4125792758089ba93f084c85
izumichika <chikara0707@outlook.jp>
2020-04-10 19:57:07 +0900

    トップページのビューを仮置き

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "tangos#index"
+  # root "tangos#index"
+  root to: "toppage#index"
   resources :toppage, only: :index
   resources :tangos, only: [:index, :create, :destroy, :show] do
     collection do
