059f032d5a0e1d9ab5cf99e3e85e70dd828c3956
izumichika <chikara0707@outlook.jp>
2020-04-10 19:54:39 +0900

    create toppage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "tangos#index"
+  resources :toppage, only: :index
   resources :tangos, only: [:index, :create, :destroy, :show] do
     collection do
       get 'drilltest', to: "tangos#drilltest"
