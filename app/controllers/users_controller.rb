6eb9c8fa097193dea7e7111f0fd7dd2f8b57bf2f
izumichika <chikara0707@outlook.jp>
2020-04-12 22:05:05 +0900

    add mypage
    ユーザーページで単語登録出来るように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
     @tangos = Tango.all.order(id: "DESC")
     @tango = Tango.new
   end
-  
+
+  # def create
+  #   @tangos = Tango.create(tango_params)
+  #   # Tango.create(tango_params)
+  #   redirect_to "/"
+  # end
+
+  # private
+  # def tango_params
+  #   params.require(:tango).permit(:question, :anser)
+  # end
+
 end
