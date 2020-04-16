6b19b53b9bf73303387ae1c4e38f473d6b73f3ae
izumichika <chikara0707@outlook.jp>
2020-04-16 20:18:17 +0900

    add Spec
    他のユーザーの登録単語はみられない

--- spec/system/tangos_spec.rb
+++ spec/system/tangos_spec.rb
@@ -27,5 +27,22 @@ describe '単語管理機能', type: :system do
         expect(page).to have_content '確認'
       end
     end
+
+    context 'ユーザーBがログインしているとき' do
+      before do
+        #ユーザーBを作成しておく
+        FactoryBot.create(:user, email: 'b@example.com')
+        #ユーザーBでログインする
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+      end
+
+      it 'ユーザーAが作成した単語が表示されない' do
+        #ユーザーAが作成したタスクの問いが画面上に表示されていないことを確認
+        expect(page).to have_no_content '確認'
+      end
+    end
   end
 end
