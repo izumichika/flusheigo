e0e274534a93630eb1f8f5a4b9eb617b6de619dc
izumichika <chikara0707@outlook.jp>
2020-04-16 20:30:40 +0900

    letで共通部分をまとめた

--- spec/system/tangos_spec.rb
+++ spec/system/tangos_spec.rb
@@ -1,46 +1,32 @@
 require 'rails_helper'
-
+# ターミナル テスト実行コマンド
+# bundle exec rspec spec/system/tangos_spec.rb
 describe '単語管理機能', type: :system do
   describe '単語表示機能' do
+    let(:user_a) { FactoryBot.create(:user, email: 'a@example.com')}
+    let(:user_b) { FactoryBot.create(:user, email: 'b@example.com')}
+
     before do
-      #ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, email: 'a@example.com')
-      #作成者がユーザーAである単語を作成しておく
       FactoryBot.create(:tango, question: '確認', user: user_a)
+
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログイン'
     end
 
     context 'ユーザーAがログインしているとき' do
-      before do
-      #ユーザーAでログインする
-        #ログイン画面にアクセスする
-        visit new_user_session_path
-        #メールアドレスを入力する
-        fill_in 'メールアドレス', with: 'a@example.com'
-        #パスワードを入力する
-        fill_in 'パスワード', with: 'password'
-        #ログインするボタンを押す
-        click_button 'ログイン'
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成した単語が表示される' do
-        #作成済みの単語の日本語と英語が画面上に表示されていることを確認
         expect(page).to have_content '確認'
       end
     end
 
     context 'ユーザーBがログインしているとき' do
-      before do
-        #ユーザーBを作成しておく
-        FactoryBot.create(:user, email: 'b@example.com')
-        #ユーザーBでログインする
-        visit new_user_session_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログイン'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成した単語が表示されない' do
-        #ユーザーAが作成したタスクの問いが画面上に表示されていないことを確認
         expect(page).to have_no_content '確認'
       end
     end
