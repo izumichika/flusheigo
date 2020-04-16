c7f27373d7b23a9f9007c681d251336c9778dad4
izumichika <chikara0707@outlook.jp>
2020-04-16 20:46:01 +0900

    フラッシュ機能のSpec

--- spec/system/tangos_spec.rb
+++ spec/system/tangos_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 # ターミナル テスト実行コマンド
 # bundle exec rspec spec/system/tangos_spec.rb
 describe '単語管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, email: 'a@example.com')}
+  let(:user_b) { FactoryBot.create(:user, email: 'b@example.com')}
+  let!(:tango_a) { FactoryBot.create(:tango, question: '確認', user: user_a)}
+
+  before do
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログイン'
+  end
+
   describe '単語表示機能' do
-    let(:user_a) { FactoryBot.create(:user, email: 'a@example.com')}
-    let(:user_b) { FactoryBot.create(:user, email: 'b@example.com')}
-
-    before do
-      FactoryBot.create(:tango, question: '確認', user: user_a)
-
-      visit new_user_session_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログイン'
-    end
-
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
@@ -28,6 +27,20 @@ describe '単語管理機能', type: :system do
 
       it 'ユーザーAが作成した単語が表示されない' do
         expect(page).to have_no_content '確認'
+      end
+    end
+  end
+
+  describe 'フラッシュ機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit flush_tangos_path(tango_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '確認'
       end
     end
   end
