99433bbf3a8e6e681ace9bb40c24ec79657df7f3
izumichika <chikara0707@outlook.jp>
2020-04-16 20:01:30 +0900

    単語表示機能

--- spec/system/tangos_spec.rb
+++ spec/system/tangos_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
-describe '単語管理機能' type: :system do
+describe '単語管理機能', type: :system do
   describe '単語表示機能' do
     before do
       #ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      user_a = FactoryBot.create(:user, email: 'a@example.com')
       #作成者がユーザーAである単語を作成しておく
-      FactoryBot.create(:tango, question: '確認', anser: 'check', user: user_a)
+      FactoryBot.create(:tango, question: '確認', user: user_a)
     end
 
     context 'ユーザーAがログインしているとき' do
       before do
-        #ユーザーAでログインする
+      #ユーザーAでログインする
+        #ログイン画面にアクセスする
+        visit new_user_session_path
+        #メールアドレスを入力する
+        fill_in 'メールアドレス', with: 'a@example.com'
+        #パスワードを入力する
+        fill_in 'パスワード', with: 'password'
+        #ログインするボタンを押す
+        click_button 'ログイン'
       end
 
       it 'ユーザーAが作成した単語が表示される' do
         #作成済みの単語の日本語と英語が画面上に表示されていることを確認
+        expect(page).to have_content '確認'
       end
     end
   end
