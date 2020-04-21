30b460dd394dbabc887f6cddab40206d2f6d973b
izumichika <chikara0707@outlook.jp>
2020-04-21 22:11:28 +0900

    修正
    条件不備を修正

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,14 +1,18 @@
 class TestsController < ApplicationController
 
   def test
-    @quizzes = Tango.where(user_id: current_user.id).sample(10)
+    @tangos = Tango.where(user_id: current_user.id).sample(10)
   end
 
   def mark
     quizzes = Tango.find(params[:quiz])
-    @ansers = []
+    # @ansers = []
+    @tangos = []
+    # quizzes.each do |q|
+    #   @ansers << {quiz: q, right: q.anser?(params["quiz#{q.id}"])}
+    # end
     quizzes.each do |q|
-      @ansers << {quiz: q, right: q.anser?(params["quiz#{q.id}"])}
+      @tangos << {quiz: q, right: q.anser?(params["quiz#{q.id}"])}
     end
   end
 
